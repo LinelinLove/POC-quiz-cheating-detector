b6caec72fbfaa19d252df9c20ffcfd28722dcca5
Chrisline Lin <81916964+LinelinLove@users.noreply.github.com>
2024-02-22 23:16:24 +0100

    Update timer/script.js
    Co-authored-by: Louisan TCHITOULA <96016326+LTOssian@users.noreply.github.com>

--- timer/script.js
+++ timer/script.js
@@ -1,8 +1,8 @@
 class Timer {
   /**
    *
-   * @param {*} time value in second, duration of the timer
-   * @param {*} elementId element where the timer will be displayed
+   * @param {number} time value in second, duration of the timer
+   * @param {HTMLElement} element element where the timer will be displayed
    */
   constructor(time, elementId) {
     this.time = time;
